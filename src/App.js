d74e1b649b2285b5d444f4031392cc69e3d74a48
dhirajthakre21 <dhirajthakre21@@gmail.com>
2020-08-06 14:13:15 +0000

    Added Click on Para and Input box

--- src/App.js
+++ src/App.js
@@ -1,55 +1,85 @@
-import React , { Component } from 'react';
+import React , { useState} from 'react';
 import './App.css';
+// Functional Component 
+// We use the hooks here instead of state we saw in the class based component 
 import Person from './Person/Person.js' ; 
 import Button from 'react-bootstrap/Button';
 
 //Hooks are used in functional component while states are used in class based component . 
+// this state only exist in classes 
+// in functional states they does not exist 
 
 
-class App extends Component {
-	//The state object is where you store property values that belongs to the component
-		    //When the state object changes, the component re-renders .
-		state =  
-		{ 
+const App = ()=> {
+//The state object is where you store property values that belongs to the component
+//When the state object changes, the component re-renders .
+	const [personState , setPersonState]= useState ({ 
 		persons : [
-			{ name : 'Dhiraj' , age : '12'} , 
-			{ name : 'Akash' , age : '21'} ,
+				{ name : 'Dhiraj Thakre' , age : '21'} , 
+				{ name : 'Sandesh Vanwadi' , age : '22'} ,
 			
-						]
-		} 
-		switchNameHandler(data) {
-			console.log(data)
-		   if (this.state.persons[0].name==='Dhiraj' ) 
+				  ]
+	});
+	
+	
+	const switchNameHandler = (data) =>
+	{
+		
+		if (personState.persons[0].name==='Dhiraj Thakre' ) 
 				{
-		 		this.setState      ({
+		 		setPersonState      ({
 				persons : [
-							{ name : 'Amruta' , age : '10' } , 
-							{ name : data , age : '15'} 
+							{ name : data , age : '10' } , 
+							{ name : 'Shrikant Patil ' , age : '15'} 
 						  ]
 						            })
 				}
-		 	else 
+		else 
 				{
-				this.setState      ({
+				setPersonState      ({
 				persons : [
-							{ name : 'Dhiraj' , age : '12'} , 
-							{ name : 'Akash' , age : '21'} 
+							{ name : 'Dhiraj Thakre' , age : '21'} , 
+							{ name : 'Sandesh Vanwadi' , age : '22'} ,
 						  ]
 						            })
 			 	
 				}
+	}
+		const switchNameChanger = (event)=> 
+		{		
+				setPersonState      ({
+				persons : [
+							{ name : 'Dhiraj Thakre' , age : '21'} , 
+							{ name : event.target.value , age : '22'} 
+						  ]
+						            })
+			
+			
 		}
 
-	   
-		render() {
-		return (
+				return (
 			// pass this data to component 
 			// this refers to global object 
     		<div className="App">
-		  		<h1> Hi there here is a React App !! </h1>
-				<Button className='btn btn-danger btn-sm' onClick={()=>this.switchNameHandler('Amruta')} >Click here </Button>
-				<Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} /> 
-				<Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
+		  		<h1> Welcome to my Basic React App  !! </h1>
+						<p> </p>
+				<Button className='btn btn-danger btn-sm' onClick={switchNameHandler.bind( this , 'Akash Raut')} >Click here to change the name ! </Button>
+						<p> </p>
+				
+				<Person
+					name = {personState.persons[0].name} 
+					age = {personState.persons[0].age}  
+					 
+					/>
+				<Person 
+					name = {personState.persons[1].name} 
+					age = {personState.persons[1].age} 
+					click= {switchNameHandler.bind(this , 'Sanket')}
+					changed={switchNameChanger}
+					 />
+						
+				
+														   
     		</div>
 				)
 			
@@ -57,8 +87,7 @@ class App extends Component {
 			//Here we have created element and returned to front page on calling function App . Which is default !!
 			// the first parameter is tag , second is JavaScript object then there is a element !! 
   				
-  				
-};	};
+	};			
 
 
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,27 @@
 
 import React from 'react';
 const Person = (data) => { // this are called as props also the argument simply object !!
-	return  (
-		<p> I'm a {data.name } and I'm {data.age} old  </p> 
-			)  
- 					}
+	if(data.name==='Dhiraj Thakre' || data.name==='Akash Raut' ) 
+			{
+				return  (
+					<div>	
+					<p onClick={data.click} >   I'm a {data.name } and I'm {data.age} old </p>
+					<input type='text' onChange={data.changed }></input>
+					<p> </p>	
+					</div>
+		
+						)  }
+ 				  
+	else 
+		{
+			return( <div>	
+					<p onClick={data.click} >   I'm a {data.name } and I'm {data.age} old </p>
+					<input type='text' placeholder='Enter name to change 'onChange={data.changed }></input>
+					<p> </p>	
+					</div>
+			
+					)
+			}
+}
 
 export default Person ; 
